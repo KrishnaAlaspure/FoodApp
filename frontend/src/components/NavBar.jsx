f76c361de69f7bdb66ef240bba0162dfdadc1404
Krishna Alaspure <38836025+KrishnaAlaspure@users.noreply.github.com>
2024-04-10 23:57:00 +0530

    First Commit

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    
+     <>
+     <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav >
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+     </>
+    
+  )
+}
+
+export default NavBar
---
+++ frontend/src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <div>
+      <div><NavBar/></div>
+      <div><Footer/></div>
+    </div>
+  )
+}
+
+export default Home
